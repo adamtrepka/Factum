1746398ee8b5f62e02e62e640c395ca7b0bc025c
Adam Trepka <kleszczowiec@gmail.com>
2022-08-15 21:00:21 +0200

    feat(documents): add documents api infrastructure

---
+++ src/Modules/Documents/Factum.Modules.Documents.Api/Controllers/DocumentsController.cs
@@ -0,0 +1,26 @@
+﻿using Factum.Shared.Abstractions.Contexts;
+using Factum.Shared.Abstractions.Dispatchers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factum.Modules.Documents.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentsController : Controller
+    {
+        private readonly IDispatcher _dispatcher;
+        private readonly IContext _context;
+
+        public DocumentsController(IDispatcher dispatcher, IContext context)
+        {
+            _dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+    }
+}
---
+++ src/Modules/Documents/Factum.Modules.Documents.Api/DocumentsModule.cs
@@ -0,0 +1,33 @@
+﻿using Factum.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factum.Modules.Documents.Api
+{
+    internal class DocumentsModule : IModule
+    {
+        public static readonly string DocumentsPolicyName = "documents";
+
+        public string Name => "Documents";
+
+        public IEnumerable<string> Policies { get; } = new[]
+        {
+            DocumentsPolicyName
+        };
+
+        public void Register(IServiceCollection services)
+        {
+
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+
+        }
+    }
+}
