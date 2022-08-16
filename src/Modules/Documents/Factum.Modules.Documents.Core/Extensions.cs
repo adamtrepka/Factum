95aed73b8d170370e3bc1934856f142c7d59c64d
Adam Trepka <kleszczowiec@gmail.com>
2022-08-16 10:22:25 +0200

    feat(documents): add documents core project

---
+++ src/Modules/Documents/Factum.Modules.Documents.Core/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Factum.Modules.Documents.Api")]
+[assembly: InternalsVisibleTo("Factum.Modules.Documents.Application")]
+[assembly: InternalsVisibleTo("Factum.Modules.Documents.Infrastructure")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+namespace Factum.Modules.Documents.Core;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        return services;
+    }
+}
+
