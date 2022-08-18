4d1781ffa94aaed8aa660ffc991b21abbd955978
Adam Trepka <kleszczowiec@gmail.com>
2022-08-18 13:20:22 +0200

    feat(ledger): add ledger api project

---
+++ src/Modules/Ledger/Factum.Modules.Ledger.Api/LedgerModule.cs
@@ -0,0 +1,26 @@
+﻿using Factum.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Factum.Modules.Ledger.Api
+{
+    public class LedgerModule : IModule
+    {
+        public static readonly string LedgerPolicyName = "ledger";
+
+        public string Name => "Ledger";
+
+        public IEnumerable<string> Policies { get; } = new[]
+        {
+            LedgerPolicyName
+        };
+
+        public void Register(IServiceCollection services)
+        {
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+        }
+    }
+}
