e43cd4db3f7b7245ae71b7847a06473878180b3d
Adam Trepka <kleszczowiec@gmail.com>
2022-08-24 14:44:21 +0200

    feat(ledger): add entry added event

--- src/Modules/Ledger/Factum.Modules.Ledger.Core/Events/External/Handlers/DocumentAddedHandler.cs
+++ src/Modules/Ledger/Factum.Modules.Ledger.Core/Events/External/Handlers/DocumentAddedHandler.cs
@@ -2,6 +2,7 @@
 using Factum.Modules.Ledger.Core.Domain.Entities;
 using Factum.Modules.Ledger.Core.EF;
 using Factum.Shared.Abstractions.Events;
+using Factum.Shared.Abstractions.Messaging;
 using Microsoft.Extensions.Logging;
 
 namespace Factum.Modules.Ledger.Core.Events.External.Handlers
@@ -11,12 +12,14 @@ namespace Factum.Modules.Ledger.Core.Events.External.Handlers
         private readonly IDocumentApiClient _documentApiClient;
         private readonly ILogger<DocumentAddedHandler> _logger;
         private readonly LedgerDbContext _dbContext;
+        private readonly IMessageBroker _messageBroker;
 
-        public DocumentAddedHandler(IDocumentApiClient documentApiClient, ILogger<DocumentAddedHandler> logger, LedgerDbContext dbContext)
+        public DocumentAddedHandler(IDocumentApiClient documentApiClient, ILogger<DocumentAddedHandler> logger, LedgerDbContext dbContext, IMessageBroker messageBroker)
         {
             _documentApiClient = documentApiClient ?? throw new ArgumentNullException(nameof(documentApiClient));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _messageBroker = messageBroker ?? throw new ArgumentNullException(nameof(messageBroker));
         }
         public async Task HandleAsync(DocumentAdded @event, CancellationToken cancellationToken = default)
         {
@@ -25,6 +28,7 @@ namespace Factum.Modules.Ledger.Core.Events.External.Handlers
 
             await _dbContext.Entries.AddAsync(entry,cancellationToken);
             await _dbContext.SaveChangesAsync(cancellationToken);
+            await _messageBroker.PublishAsync(new EntryAdded(entry.BusinessId),cancellationToken);
 
             _logger.LogInformation($"Created a new entry based on document with ID: '{@event.documentId}'.");
         }
