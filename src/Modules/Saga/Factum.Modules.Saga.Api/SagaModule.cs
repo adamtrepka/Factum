b8b31e4cbdaed1445bb7eaa3b7f154f1bd596f06
Adam Trepka <kleszczowiec@gmail.com>
2022-08-25 11:41:11 +0200

    feat(saga): add saga api project

---
+++ src/Modules/Saga/Factum.Modules.Saga.Api/SagaModule.cs
@@ -0,0 +1,25 @@
+﻿using Chronicle;
+using Factum.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Factum.Modules.Saga.Api
+{
+    internal class SagaModule : IModule
+    {
+        public string Name => "Saga";
+        public IEnumerable<string> Policies { get; } = new[]
+        {
+            "saga"
+        };
+
+        public void Register(IServiceCollection services)
+        {
+            services.AddChronicle();
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+        }
+    }
+}
