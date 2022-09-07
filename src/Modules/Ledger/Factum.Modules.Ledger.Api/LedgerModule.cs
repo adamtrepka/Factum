55d0988cf9f1d5594357853286b0513a48f8a3f1
Adam Trepka <kleszczowiec@gmail.com>
2022-09-07 14:33:30 +0200

    feat(ledger): add event handler for revoked access

--- src/Modules/Ledger/Factum.Modules.Ledger.Api/LedgerModule.cs
+++ src/Modules/Ledger/Factum.Modules.Ledger.Api/LedgerModule.cs
@@ -40,7 +40,8 @@ namespace Factum.Modules.Ledger.Api
                .Register<BlockRejectedContract>()
                .Register<SagaComplatedContract>()
                .Register<SagaRejectedContract>()
-               .Register<DocumentAccessGrantedContract>();
+               .Register<DocumentAccessGrantedContract>()
+               .Register<DocumentAccessRevokedContract>();
 
             app.UseModuleRequests()
                .Subscribe<GetBlock, BlockDetailsDto>("ledger/get",
---
+++ src/Modules/Ledger/Factum.Modules.Ledger.Application/Entries/Events/External/DocumentAccessRevoked.cs
@@ -0,0 +1,22 @@
+﻿using Factum.Shared.Abstractions.Contracts;
+using Factum.Shared.Abstractions.Events;
+using Factum.Shared.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factum.Modules.Ledger.Application.Entries.Events.External
+{
+    internal record DocumentAccessRevoked(Guid AccessId, Guid DocumentId, string AccessType, Guid RevokedBy, Guid RevokedTo) : IEvent;
+
+    [Message("documents")]
+    internal class DocumentAccessRevokedContract : Contract<DocumentAccessRevoked>
+    {
+        public DocumentAccessRevokedContract()
+        {
+            RequireAll();
+        }
+    }
+}
