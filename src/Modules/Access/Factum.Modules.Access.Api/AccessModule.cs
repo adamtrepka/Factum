77e9b932857ee8a962a719ee867d19e7b178d9d8
Adam Trepka <kleszczowiec@gmail.com>
2022-09-14 10:10:58 +0200

    feat(access): add access module projects

---
+++ src/Modules/Access/Factum.Modules.Access.Api/AccessModule.cs
@@ -0,0 +1,27 @@
+﻿using Factum.Modules.Access.Core;
+using Factum.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Factum.Modules.Access.Api
+{
+    public class AccessModule : IModule
+    {
+        public string Name => "Access";
+
+        public IEnumerable<string> Policies { get; } = new[]
+        {
+            "access"
+        };
+
+        public void Register(IServiceCollection services)
+        {
+            services.AddCore();
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+            
+        }
+    }
+}
---
+++ src/Modules/Access/Factum.Modules.Access.Core/Extensions.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Factum.Modules.Access.Api")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+namespace Factum.Modules.Access.Core
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
